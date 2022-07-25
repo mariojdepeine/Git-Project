124afe747db72cf8bf331b80934e0021c50c5cb7
mariojdepeine <30647342+mariojdepeine@users.noreply.github.com>
2022-07-26 01:49:43 +0900

    changed link back to blue

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                     Text("This is the Content View")
                     NavigationLink(destination: ModalView()){
                         Text("Go Next")
-                    }.foregroundColor(.purple)
+                    }.foregroundColor(.blue)
                         .navigationBarTitle("Xcode and Git")
                 }
         }
